9056ddc24ba0a585522df485497d1311d265ef3c
Malik <malik.arykov@mail.ru>
2025-07-13 23:52:34 +0300

    добавил инструкцию пользователя (значок ? в правом верхнем углу)

--- classmates.js
+++ classmates.js
@@ -15,12 +15,13 @@ backlights.forEach((backlight) => {
  */
 function showPopup(divElement) {
     let imageSrc = './img/' + divElement.id || ''; // имя файла с фото берем из ID
-    let popupText = divElement.dataset.text || ''; // описание фото - из data-text
+    let popupText = divElement.dataset.text || getHelp(); // описание фото - из data-text
 
     // устанавливаем полученные значения в соответствующее место
     let popupInfo = document.getElementById('popup-info');
     let imgElement = popupInfo.querySelector('img');
-    let textElement = popupInfo.querySelector('p');
+    // let textElement = popupInfo.querySelector('p');
+    let textElement = popupInfo.querySelector('figcaption');
     imgElement.src = imageSrc;
     textElement.innerText = popupText;
 
@@ -35,4 +36,17 @@ function hidePopup() {
     // скрываем всплывающее окно
     let popupInfo = document.getElementById('popup-info');
     popupInfo.style.display = 'none';
+}
+
+/**
+ * Получить инструкцию пользователя
+ */
+function getHelp() {
+    const help = `При клике (тапе) по подкрашенной фотографии
+    одноклассника, появится его/ее более крупное современное фото
+    с комментарием (Имя Фамилия - Год фото. Город проживания).
+    Если желаете, чтобы высвечивалось ваше фото, 
+    то присылайте его с комментарием на malik.arykov@mail.ru.
+    `
+    return help;
 }
